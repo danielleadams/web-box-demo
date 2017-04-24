c632203547e3c248be0a6165751a7e981670fa81
Danielle Adams <adamzdanielle@gmail.com>
2017-04-24 13:32:22 -0700

    Add labels index page

--- app/controllers/labels_controller.rb
+++ app/controllers/labels_controller.rb
@@ -15,6 +15,12 @@ class LabelsController < ApplicationController
     PrinterWorker.perform_async label.zpl
   end
 
+  def index
+    @created_labels = Label.where(status: :created)
+    @printed_labels = Label.where(status: :printed)
+    @scanned_labels = Label.where(status: :scanned)
+  end
+
   private
 
   def label_params
